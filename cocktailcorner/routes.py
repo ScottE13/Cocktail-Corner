c536418a758f9888204edaea05dc1d3feea19382
ScottE13 <>
2023-08-14 15:54:50 +0000

    Add registration functionality

--- cocktailcorner/routes.py
+++ cocktailcorner/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import flash, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from cocktailcorner import app, db
 from cocktailcorner.models import User, Category, Cocktail
 
@@ -10,4 +11,26 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check to see if the username already exists
+        existing_user = User.query.filter(User.username ==
+                                          request.form.get(
+                                            "username").lower()).all()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        current_user = User(
+            username=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(current_user)
+        db.session.commit()
+
+        # Add user to the session
+        session["current_user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("register", username=session["current_user"]))
     return render_template("register.html")
