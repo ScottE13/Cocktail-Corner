ce7333050bbf50ffc2282006d272c847efb58836
ScottE13 <>
2023-08-15 15:59:26 +0000

    Initialize form select list

--- cocktailcorner/static/js/script.js
+++ cocktailcorner/static/js/script.js
@@ -2,4 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialize mobile sidenav
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Initialize form select list
+    let selectlist = document.querySelectorAll('select');
+    M.FormSelect.init(selectlist);
 });
+
+
+    
